6bd2cbced6338e754fa2179723d2c81462b4f4bd
apades <apad2@qq.com>
2023-03-09 17:00:39 +0800

    init: 初始化项目

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,3 @@
+export { default as AsyncLock } from './AsyncLock'
+export { createAxios, default as axios } from './axios'
+export { default as IndexedDB2 } from './IndexedDB2'
---
+++ packages/utils/src/styleUtils.ts
@@ -0,0 +1,20 @@
+import path from 'path'
+import type { CSSProperties } from 'react'
+
+export function getMixinLessPath() {
+  return path.resolve(__dirname, './mixin.less')
+}
+
+export function getClientRect<T extends HTMLElement>(dom: T): DOMRect {
+  let rect: DOMRect
+  try {
+    rect = dom?.getClientRects()[0]
+  } catch (error) {
+    console.error('some error', error)
+  }
+  return rect
+}
+
+export function style(style: Record<string, any> & CSSProperties) {
+  return style
+}
