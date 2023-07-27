f954854479a79718f6c2a1579359a3df8d55b288
apades <36807043+apades@users.noreply.github.com>
2023-07-27 17:41:22 +0800

    chore: 整理packages

--- packages/setting-panel/vite.config.js
+++ packages/setting-panel/vite.config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 const pr = (...p) => path.resolve(__dirname, ...p)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      gzipSize: true,
-      brotliSize: true,
-      emitFile: false,
-      filename: 'analyzer.html', //分析图生成的文件名
-      open: true, //如果存在本地服务端口，将在打包后自动展示
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@pkgs': pr('../'),
