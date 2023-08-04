0c7c2fb3a513547c3e4ef4b930f481769bce289d
apades <36807043+apades@users.noreply.github.com>
2023-08-04 17:39:34 +0800

    todo: exm

---
+++ packages/setting-panel-example/vite.config.js
@@ -0,0 +1,27 @@
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+const pr = (...p) => path.resolve(__dirname, ...p)
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@pkgs': pr('../'),
+      'preact/hooks': pr('node_modules/react'),
+      'preact/compat': pr('node_modules/react'),
+      preact: pr('node_modules/react'),
+      entry: pr('./src/entry-react'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        charset: false,
+        additionalData: '@import "../utils/src/mixin.less";',
+        javascriptEnabled: true,
+      },
+    },
+  },
+})
