77470cc6311eb2af2e30db00f12b9203ee013684
apades <36807043+apades@users.noreply.github.com>
2023-10-08 15:53:42 +0800

    exm: 增加injector ext-cs的例子

---
+++ packages/injector_test-ext-cs/config.ts
@@ -0,0 +1,9 @@
+import { InitConfig } from '@pkgs/injector/src/feats/entry/types'
+
+export const injectorConfig: InitConfig = {
+  eval: true,
+  domEvents: true,
+  triggerEvents: true,
+  fetch: true,
+  route: true,
+}
--- packages/injector_test-ext-cs/contents/inject.ts
+++ packages/injector_test-ext-cs/contents/inject.ts
@@ -1,12 +1,13 @@
-import { initClient } from '@pkgs/injector/src/core/client'
+import { initInjector } from '@pkgs/injector/src/core/injector'
 import type { PlasmoCSConfig } from 'plasmo'
 import { injectorConfig } from '../config'
 
-window.injector = initClient(injectorConfig)
-console.log('window.injector', window.injector)
+window.injectorService = initInjector(injectorConfig)
+console.log('window.injectorService', window.injectorService)
 
 export const config: PlasmoCSConfig = {
   matches: ['<all_urls>'],
+  world: 'MAIN',
   run_at: 'document_start',
   all_frames: true,
 }
